12176ec26daf4e6993a852650be00511da50d1ff
chrisbodhi <cboette@gmail.com>
2025-11-29 13:54:03 -0500

    A tad of UI touch-up.

--- crates/web/src/app.rs
+++ crates/web/src/app.rs
@@ -13,13 +13,17 @@ pub fn App() -> impl IntoView {
 
     view! {
         <Stylesheet id="leptos" href="/pkg/web.css"/>
-        <Title text="Actuator Plate Configurator???"/>
+        <Title text="Brighton Actuation Systems"/>
         <Router>
             <main>
                 <Routes fallback=|| "Page not found.">
                     <Route path=StaticSegment("") view=HomePage/>
                 </Routes>
             </main>
+            <footer class="flex justify-between">
+                <div>"©" 2025 Brighton Actuation Systems</div>
+                <div>made with "🧑‍🏭" in PGH</div>
+            </footer>
         </Router>
     }
 }
@@ -28,7 +32,7 @@ pub fn App() -> impl IntoView {
 fn HomePage() -> impl IntoView {
     view! {
         <div class="container">
-            <h1 class="text-red-500">Actuator Plate Configurator!</h1>
+            <h1 class="text-red-500">Actuator Plate Configurator</h1>
                 <div class="container columns-2xs">
                     <img src="https://i.pinimg.com/originals/35/c0/2b/35c02b534cdbacbea92ae64ee3fe0a1d.png" alt="Cat CAD" />
                     <PlateForm/>
