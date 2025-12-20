5ea11928d9b7cdeed9cd034d4e627b465efe125f
chrisbodhi <cboette@gmail.com>
2025-12-20 14:00:42 -0500

    Start creating plate from user specs.

--- crates/parametric/src/lib.rs
+++ crates/parametric/src/lib.rs
@@ -1,3 +1,5 @@
+use std::process::ExitStatus;
+
 use domain::ActuatorPlate;
 use validation;
 
@@ -12,13 +14,73 @@ pub enum ValidationError {
     NoStep,
 }
 
-pub fn generate_step(plate: ActuatorPlate) -> Result<(), ValidationError> {
+#[derive(Debug, PartialEq)]
+pub enum GeneratorError {
+    CliError,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum AllErrors {
+    GeneratorError,
+    ValidationError,
+}
+
+fn generate_params_file(plate: &ActuatorPlate) -> std::io::Result<()> {
+    let content = format!(
+        "@settings(defaultLengthUnit = mm, kclVersion = 1.0)\n\n\
+         export const plateThickness = {:?}\n\
+         export const boltDiameter = {:?}\n\
+         export const boltSpacing = {:?}\n\
+         export const bracketHeight = {:?}\n\
+         export const pinDiameter = {:?}",
+        plate.plate_thickness,
+        plate.bolt_diameter,
+        plate.bolt_spacing,
+        plate.bracket_height,
+        plate.pin_diameter
+    );
+    std::fs::write("params.kcl", content)?;
+
+    Ok(())
+}
+
+pub fn generate_model(plate: &ActuatorPlate) -> Result<(), AllErrors> {
+    if let Err(e) = validation::validate(&plate) {
+        eprintln!("oops: {}", e);
+        return Err(AllErrors::ValidationError);
+    }
+
+    if let Err(e) = generate_params_file(&plate) {
+        eprintln!("oops on that param: {}", e);
+        return Err(AllErrors::GeneratorError);
+    }
+
+    Ok(())
+}
+
+pub fn generate_step(plate: ActuatorPlate) -> Result<ExitStatus, ValidationError> {
     if let Err(e) = validation::validate(&plate) {
         eprintln!("oops: {}", e);
         return Err(ValidationError::NoStep);
     }
 
-    Ok(())
+    let a = std::process::Command::new("zoo")
+        .args(&[
+            "kcl",
+            "export",
+            "--output-format=step",
+            "main.kcl", // TODO: create this main.kcl
+            "output_dir",
+        ])
+        .status();
+
+    match a {
+        Ok(stat) => Ok(stat),
+        Err(e) => {
+            eprintln!("ouch: {}", e);
+            return Err(ValidationError::NoStep);
+        }
+    }
 }
 
 #[cfg(test)]
